4009806c51f2e633cd6ac6fc827713f8eae6a7ef
Guillermina.Aviglian <guillermina.avigliano@endava.com>
2019-09-19 15:26:47 -0300

    create modules

---
+++ src/app/contacts/contacts.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { ContactsRoutingModule } from './contacts-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    ContactsRoutingModule,
+    SharedModule,
+  ]
+})
+export class ContactsModule { }
